7894fb65b14c96885a546330452b84b186c5d3b2
suha-edris <suha.edris@gmail.com>
2017-06-27 14:53:40 +0300

    setup handlebars

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const hbs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
+
+app.engine('hbs', hbs({
+  defaultLayout: 'main',
+  deaultDir: path.join(__dirname, './', 'views/layouts'),
+  partialsDir: path.joing(__dirname, './', 'views/partials')
+  extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './', 'views'));
 
 app.get('/', (req, res) => {
   res.send('Hello World')
