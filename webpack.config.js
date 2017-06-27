4b9d9edeaf7c2565c69d91c4386d1b15a3f6aec2
suha-edris <suha.edris@gmail.com>
2017-06-27 18:43:21 +0300

    add js loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+
 module.exports = {
-  entry: './src/server.js',
+  entry: path.join(__dirname, './', 'src/server.js'),
   output: {
     path: path.resolve(__dirname, './public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.hbs$/,
         exclude: /node_modules/,
-        loader: 'handlebars-loader'
+        use: [
+          {loader: 'handlebars-loader'},
+        ]
     },
     {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+        use: [
+          {loader: 'babel-loader',
           options: {
             presets: ['env']
           }
-        }
+        }]
     }
   ]
   },
