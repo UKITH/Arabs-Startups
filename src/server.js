ea7a0d32112ec6c083a35a7ad5dadfadd7812afa
suha-edris <suha.edris@gmail.com>
2017-06-27 15:31:31 +0300

    fix handlebars setup

--- src/server.js
+++ src/server.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 const path = require('path');
+const router = require('./routes/index.js');
 
 const app = express();
 
-app.engine('hbs', hbs({
-  defaultLayout: 'main',
-  deaultDir: path.join(__dirname, './', 'views/layouts'),
-  partialsDir: path.joing(__dirname, './', 'views/partials')
-  extname: 'hbs'
-}));
+app.engine('hbs', hbs({}));
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, './', 'views'));
+app.set('views', path.join(__dirname, './', '/views'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-});
+app.use(router);
 
 module.exports = app;
