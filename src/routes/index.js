c868a3f58e406b4c1212ab3f10634492cc169e7d
suha-edris <suha.edris@gmail.com>
2017-06-28 17:24:38 +0300

    add route startup profile page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const searchHome = require('./search_home.js');
+const startupProfile = require('./startup_profile.js')
 
 router.get('/', searchHome);
+router.get('/Startup_Profile', startupProfile);
 
 module.exports = router;
