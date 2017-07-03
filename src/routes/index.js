1350a56b6df5e72d5311b27fa26d27db314e2089
suha-edris <suha.edris@gmail.com>
2017-07-03 12:11:40 +0300

    remove startupProfile router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 
 const searchHome = require('./search_home.js');
-const startupProfile = require('./startup_profile.js')
 
 router.get('/', searchHome);
-router.get('/startupProfile', startupProfile);
 
 module.exports = router;
