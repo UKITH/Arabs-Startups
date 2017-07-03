bb1b3150fcd853ebf0d8061f4992fc9833e95ab3
Mario <mario.saliba98@gmail.com>
2017-07-04 00:41:45 +0300

    add a route for the result page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const searchHome = require('./search_home.js');
 const submissionForm = require('./submission_form.js');
 const registerStartup = require('./register_startup.js');
+const searchResults = require('./search_results');
 
 router.get('/', searchHome);
 router.get('/submissionForm', submissionForm);
+router.get('/results', searchResults);
 router.post('/registerStartup', registerStartup);
 
 module.exports = router;
