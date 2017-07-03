d783b116b2d1338ccfc0f077b2f96de697fe2083
suha-edris <suha.edris@gmail.com>
2017-07-03 14:55:45 +0300

    add temporary mock data

---
+++ src/mock_data.js
@@ -0,0 +1,32 @@
+module.exports = {
+  [
+    {
+      _id: "1",
+      founder-name: "Mario Saliba",
+      startup-name: "MarioObsession",
+      phone-number: "0528598742",
+      mail: "mario.saliba98@gmail.com",
+      startup-field: "software",
+      startup-description: "blah blah blah",
+      startup-stage: "prototype",
+      website-link: "www.mario.com",
+      facebook-link: "www.facebook.com",
+      linkedIn-link: "www.linkedin.com",
+      twitter-link: "www.twitter.com"
+    },
+    {
+      _id: "2",
+      founder-name: "Suuuha",
+      startup-name: "weeee",
+      phone-number: "0528222252",
+      mail: "suuuuuh@gmail.com",
+      startup-field: "software",
+      startup-description: "blah blah blah",
+      startup-stage: "prototype",
+      website-link: "www.suha.com",
+      facebook-link: "www.facebook.com",
+      linkedIn-link: "www.linkedin.com",
+      twitter-link: "www.twitter.com"
+    }
+  ]
+}
