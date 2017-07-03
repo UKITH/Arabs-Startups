98d279ee112812f2fd31cea870d428e90bacfd34
Mario <mario.saliba98@gmail.com>
2017-07-03 15:27:39 +0300

    enables body-parser in express

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const path = require('path');
 const router = require('./routes/index.js');
 
 const app = express();
 
 app.engine('hbs', hbs({}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, './', '/views'));
