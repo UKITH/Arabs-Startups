32628e8616fa127ecd1d7bdcd5773ba4beaba181
suha-edris <suha.edris@gmail.com>
2017-07-04 13:06:42 +0300

    fix connecction to database

--- src/start_server.js
+++ src/start_server.js
@@ -1,3 +1,13 @@
 const app = require('./server.js');
 
-app.listen(4040);
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://suha:shukran1@ds139082.mlab.com:39082/arab-startups');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  // We are connected
+  console.log('connected to db');
+  app.listen(4040);
+});
