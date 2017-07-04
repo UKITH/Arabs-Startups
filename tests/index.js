98dc00731a78b1b7ccec76996f5380cae9e1b056
Mario <mario.saliba98@gmail.com>
2017-07-04 11:36:33 +0300

    add basic tests

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,7 @@
 var test = require('tape');
 
+const serverSide = require('./server/index.js');
+
 test('should return -1 when the value is not present in Array', function (t) {
   t.equal(-1, -1);
   t.end();
