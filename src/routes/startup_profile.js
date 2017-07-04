171e20de0b006337d014e145be735b63156abd57
suha-edris <suha.edris@gmail.com>
2017-07-04 17:17:18 +0300

    change error handling

--- src/routes/startup_profile.js
+++ src/routes/startup_profile.js
@@ -3,10 +3,11 @@ const { mockCollection } = require('../../database/schema.js');
 module.exports = (req, res) => {
   const id = req.params.id;
   mockCollection.findOne({_id:id}, (error, startup)=> {
-    if(error) res.render(error);
+    if(error) res.render('not_found');
     else {
       res.render('startup_profile', {
-          startup: startup
+          startup: startup,
+          back: req.headers.referer
       })
     }
   })
