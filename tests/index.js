76c37a96dec6342708953cc06e8ad08c9085e8c7
Mario <mario.saliba98@gmail.com>
2017-07-05 11:49:23 +0300

    get rid of basic test

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,1 @@
-var test = require('tape');
-
 const serverSide = require('./server/index.js');
-
-test('should return -1 when the value is not present in Array', function (t) {
-  t.equal(-1, -1);
-  t.end();
-});
