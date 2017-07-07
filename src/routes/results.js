d038971c102804e0635cd051ce524e5ade6438f6
Mario <mario.saliba98@gmail.com>
2017-07-07 20:10:41 +0300

    change the functionality of the results page

--- src/routes/results.js
+++ src/routes/results.js
@@ -1,22 +1,15 @@
 const { mockCollection } = require('../../database/schema.js');
 
 module.exports = (req, res) => {
-  searchWord = req.query.search;
-  stage = Object.keys(req.query)[1];
+  var options = {};
+  const filterArray = [
+    req.query.search ? 'search' : 0,
+    req.query.stage ? 'stage' : 0,
+    req.query.sector ? 'sector' : 0
+  ].join('')
 
-  if (searchWord) {
-    mockCollection.find({"startupName": searchWord}, (error, startupFound) => {
-      if (error) res.render('not_found')
-      else {
-        res.render('search_results', {
-          startups: startupFound
-        })
-      }
-    })
-  }
-
-  if(stage) {
-    mockCollection.find({"fundingStage": stage}, (error, startups) => {
+  function findAll(options) {
+    mockCollection.find(options, (error, startups) => {
       if (error) res.render('not_found')
       else {
         res.render('search_results', {
@@ -26,9 +19,61 @@ module.exports = (req, res) => {
     })
   }
 
-  // this is here so ican test the results page
-  // when we have the search engine i'll remove it
-  if(!searchWord && !stage){
-    res.render('search_results')
+  switch (filterArray) {
+    case 'searchstagesector':
+       options = {
+        startupName: req.query.search,
+        startupSector: req.query.sector,
+        fundingStage: req.query.stage
+      }
+      findAll(options);
+    break;
+
+    case 'searchstage0':
+       options = {
+        startupName: req.query.search,
+        fundingStage: req.query.stage
+      }
+      findAll(options);
+    break;
+
+    case '00sector':
+       options =  {
+        startupSector: req.query.sector,
+      }
+      findAll(options);
+    break;
+    // there needs to be validation for the search input in the front end so this would fully work without any bugs
+    case 'search00':
+       options = {
+        startupName: req.query.search,
+      }
+      findAll(options);
+    break;
+
+    case '0stagesector':
+       options = {
+        fundingStage: req.query.stage,
+        startupSector: req.query.sector
+      }
+      findAll(options);
+    break;
+
+    case 'search0sector':
+      options = {
+        startupName: req.query.search,
+        startupSector: req.query.sector
+      }
+      findAll(options);
+    break;
+
+    case '0stage0':
+      options = {
+        fundingStage: req.query.stage,
+      }
+      findAll(options);
+    break;
+
+    default: res.render('search_results')
   }
 }
