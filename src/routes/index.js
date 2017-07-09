14c08d7763a0768dd926fa4efbbb1e2a0d25610f
suha-edris <suha.edris@gmail.com>
2017-07-09 14:47:21 +0300

    add route for submit message view

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ const searchHome = require('./search_home.js');
 const submissionForm = require('./submission_form.js');
 const registerStartup = require('./register_startup.js');
 const startupProfile = require('./startup_profile.js');
+const submitMessage = require('./submit_message.js');
 
 
 router.get('/', searchHome);
 router.get('/submissionForm', submissionForm);
 router.post('/registerStartup', registerStartup);
 router.get(`/startupProfile/:id`, startupProfile);
+router.get('/submitMessage', submitMessage);
 
 module.exports = router;
