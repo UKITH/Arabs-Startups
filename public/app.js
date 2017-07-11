4adee396ab7f7c1d144533af447ae7bcd0ff597b
Mario <mario.saliba98@gmail.com>
2017-07-11 23:58:22 +0300

    add the functionality for the co founder option

---
+++ public/app.js
@@ -0,0 +1,7 @@
+
+
+var plusCircle = document.querySelector('#plus-circle');
+var coFounderInput = document.querySelector('#coFounder-name');
+plusCircle.addEventListener('click', function () {
+  coFounderInput.classList.toggle('dn');
+})
