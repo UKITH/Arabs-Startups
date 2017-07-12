7d792acddb2b44385a21d241280d3cf6d47bc790
Mario <mario.saliba98@gmail.com>
2017-07-12 21:10:26 +0300

    change radios to checkboxes

--- public/app.js
+++ public/app.js
@@ -8,6 +8,7 @@ sectorLabel.addEventListener('click', function() {
 var fundingLabel = document.querySelector('#funding-label');
 var fundingFieldset = document.querySelector('#funding-fieldset');
 fundingLabel.addEventListener('click', function() {
+  fundingFieldset.classList.toggle('dn');
 })
 
 var plusCircle = document.querySelector('#plus-circle');
