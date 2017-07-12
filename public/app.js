183cc9e696149d7007d5b40943bfed52bb1ea962
Mario <mario.saliba98@gmail.com>
2017-07-12 18:28:10 +0300

    make the sector and funding filters clickable

--- public/app.js
+++ public/app.js
@@ -1,4 +1,15 @@
 
+var sectorLabel = document.querySelector('#sector-label');
+var sectorFieldset = document.querySelector('#sector-fieldset');
+sectorLabel.addEventListener('click', function() {
+  sectorFieldset.classList.toggle('dn');
+})
+
+var fundingLabel = document.querySelector('#funding-label');
+var fundingFieldset = document.querySelector('#funding-fieldset');
+fundingLabel.addEventListener('click', function() {
+  fundingFieldset.classList.toggle('dn');
+})
 
 var plusCircle = document.querySelector('#plus-circle');
 var coFounderInput = document.querySelector('#coFounder-name');
