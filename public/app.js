800882ec8d204cbfbe19396dc12bfd2c84158769
Mario <mario.saliba98@gmail.com>
2017-07-19 17:04:52 +0300

    remove the dom code that wasnt being used

--- public/app.js
+++ public/app.js
@@ -1,10 +1,3 @@
-
-var sectorLabel = document.querySelector('#sector-label');
-var sectorFieldset = document.querySelector('#sector-fieldset');
-sectorLabel.addEventListener('click', function() {
-  sectorFieldset.classList.toggle('dn');
-})
-
 var fundingLabel = document.querySelector('#funding-label');
 var fundingFieldset = document.querySelector('#funding-fieldset');
 fundingLabel.addEventListener('click', function() {
