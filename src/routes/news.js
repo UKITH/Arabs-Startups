d5862733656de182e48bf724a9fae99d967f4306
Mario <mario.saliba98@gmail.com>
2017-07-21 07:48:20 +0300

    adds the functionality when going to a certain news rel #104

---
+++ src/routes/news.js
@@ -0,0 +1,13 @@
+const { mockNews } = require('../../database/news_schema.js');
+
+module.exports = (req, res) => {
+  const id = req.params.id;
+  mockNews.findOne({_id:id}, (error, news) => {
+    if (error) res.render('not_found')
+    else {
+      res.render('news.hbs', {
+        news: news
+      })
+    }
+  })
+}
