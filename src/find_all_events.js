453ab18c9549b188f6ab8359e68aa2431374ffc3
suha-edris <suha.edris@gmail.com>
2017-07-24 15:22:31 +0300

    build findAllEvents function

---
+++ src/find_all_events.js
@@ -0,0 +1,25 @@
+const { mockEvents } = require('../database/events_schema.js');
+
+const findAllEvents = (hbs, response) => {
+  mockEvents.find({}, (error, events) => {
+    if (error) response.render('not_found')
+    else {
+      let upcomingEvents = [];
+      let pastEvents = [];
+      events.forEach((event) => {
+        if(event.eventDate > new Date()) {
+          upcomingEvents.push(event)
+        }
+        else {
+          pastEvents.push(event)
+        }
+      })
+      response.render(hbs, {
+         upcomingEvents,
+         pastEvents
+      })
+    }
+  })
+}
+
+module.exports = findAllEvents;
