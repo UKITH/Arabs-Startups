0abdea72ac9c3c39eaa503736dd8e087d843759a
Mario <mario.saliba98@gmail.com>
2017-07-25 14:44:39 +0300

    adds tests for all the routes rel #111

---
+++ src/actions/find_all_startups.js
@@ -0,0 +1,14 @@
+const { mockCollection } = require('../../database/startup_schema.js');
+
+const findAllStartups = (options, hbs, response) => {
+  mockCollection.find(options, (error, startups) => {
+    if (error) response.render('not_found')
+    else {
+      response.render(hbs, {
+        startups: startups
+      })
+    }
+  })
+}
+
+module.exports = findAllStartups;
