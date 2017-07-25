8570021fa34617055d8a3244cdf5f7c8051551de
Mario <mario.saliba98@gmail.com>
2017-07-25 16:12:21 +0300

    modularise the code in the event route

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,13 +1,5 @@
-const { mockEvents } = require('../../database/events_schema.js');
+const findEvent = require('../actions/find_event.js');
 
 module.exports = (req, res) => {
-  const id = req.params.id;
-  mockEvents.findOne({_id:id}, (error, event)=> {
-    if(error) res.render('not_found');
-    else {
-      res.render('event', {
-          event: event
-      })
-    }
-  })
+  findEvent(req.params.id, 'event', res);
 }
