2668f45e459ad7a4d0a7be2224af0155b06c2f72
Mario <mario.saliba98@gmail.com>
2017-07-27 16:34:00 +0300

    adds a helper for the news page for the date rel #115

--- src/server.js
+++ src/server.js
@@ -3,16 +3,22 @@ const hbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
+
 const router = require('./routes/index.js');
+const DateToString = require('./helpers/date_to_string.js');
 
 const app = express();
 
 app.engine('hbs', hbs({
   defaultLayout: 'main',
-  layoutsDir: path.join(__dirname, './', 'views/layouts'),
-  partialsDir: path.join(__dirname, './', 'views/partials'),
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  partialsDir: path.join(__dirname, 'views/partials'),
+  helpers: {
+    DateToString,
+  },
   extname: 'hbs'
 }));
+
 app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
