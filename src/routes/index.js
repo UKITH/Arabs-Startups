d54c74127dfa621c1524350ed548aa9cd1354a3c
Mario <mario.saliba98@gmail.com>
2017-07-29 19:03:38 +0300

    adds routes for the admin pages rel #123

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,23 @@ const registerStartup = require('./register_startup.js');
 const resultsHandler = require('./results.js');
 const startupProfile = require('./startup_profile.js');
 const submitMessage = require('./submit_message.js');
+const adminLogin = require('./admin_login.js');
+const adminHome = require('./admin_home.js');
+const newsForm = require('./admin_news_form.js');
+const eventForm = require('./admin_event_form.js');
+const registerNews = require('./register_news.js');
+const registerEvent = require('./register_event.js');
 
 router.get('/', searchHome);
 router.post('/registerStartup', registerStartup);
 router.get(`/startupProfile/:id`, startupProfile);
 router.get('/search', resultsHandler);
 router.get('/submitMessage', submitMessage);
+router.get('/ukithAdmin', adminLogin);
+router.get('/u1k2i3t4h5/adminHome', adminHome);
+router.get('/u1k2i3t4h5/newsForm', newsForm);
+router.get('/u1k2i3t4h5/eventForm', eventForm);
+router.post('/u1k2i3t4h5/registerNews', registerNews);
+router.post('/u1k2i3t4h5/registerEvent', registerEvent);
 
 module.exports = router;
---
+++ src/routes/register_news.js
@@ -0,0 +1,3 @@
+module.exports = (req ,res) => {
+  res.redirect('/u1k2i3t4h5/adminHome')
+}
