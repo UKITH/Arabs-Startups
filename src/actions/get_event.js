198be5d583df48821973476e96b92cf72071eb37
suha-edris <suha.edris@gmail.com>
2017-08-03 13:05:14 +0300

    render address map coordinates

--- src/actions/get_event.js
+++ src/actions/get_event.js
@@ -1,11 +1,18 @@
 const { mockEvents } = require('../../database/events_schema.js');
+const latLng = require('../helpers/latlng.js');
 
 const getEvent = (id, hbs, res) => {
   mockEvents.findOne({_id:id}, (error, event)=> {
     if(error) res.render('not_found');
     else {
-      res.render(hbs, {
-          event: event
+      latLng(event.eventAddress, (err, coords) => {
+        if (err) res.render('not_found')
+        else {
+          res.render(hbs, {
+            event: event,
+            latlng: coords,
+          })
+        }
       })
     }
   })
