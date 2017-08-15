f732e44d6b43982746c90b856572e59f722b7df1
Mario <mario.saliba98@gmail.com>
2017-08-15 11:00:37 +0300

    adds basic burger setup rel #158

--- public/form.js
+++ public/form.js
@@ -4,5 +4,10 @@ plusCircle.addEventListener('click', function () {
   coFounderInput.classList.toggle('dn');
 })
 
+if (screen.width < 500) {
+  document.querySelector('#navs').classList.toggle('dtc');
+  document.querySelector('#navs').classList.toggle('dn');
+}
+
 var bgChangeColor = document.querySelector('#navbar');
 bgChangeColor.style.backgroundColor = "white";
