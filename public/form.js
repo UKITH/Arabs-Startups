7b88d3b4d673cf97766c26b74e7d3e560fe8ad26
suha-edris <suha.edris@gmail.com>
2017-08-16 16:51:22 +0300

    change coFoudner id name

--- public/form.js
+++ public/form.js
@@ -1,11 +1,11 @@
 var plusCircle = document.querySelector('#plus-circle');
-var coFounderInput = document.querySelector('#coFounderTest');
-// console.log(childNumber);
+var coFounderInput = document.querySelector('#co-founder');
+var childNumber = 0;
 plusCircle.addEventListener('click', function () {
-  var childNumber = document.getElementById('coFounderTest').childElementCount;
+  var childNumber = document.getElementById('co-founder').childElementCount;
     if(childNumber <= 4) {
-      var input = document.createElement('INPUT');
-      coFounderInput.appendChild(input);
+      var input = document.createElement('input');
+      coFounderInput.appendChild(input).classList.add('bg-light-gray','ba', 'b--black-20','pa2','mb2','w-100','br2','db');
       console.log(childNumber);
 
     }
