1f4cc7bab6d18d295b78ce9435d91fe7b886798b
suha-edris <suha.edris@gmail.com>
2017-08-16 17:29:15 +0300

    add id and name to createElement input

--- public/form.js
+++ public/form.js
@@ -5,6 +5,8 @@ plusCircle.addEventListener('click', function () {
   var childNumber = document.getElementById('co-founder').childElementCount;
     if(childNumber <= 4) {
       var input = document.createElement('input');
+      input.setAttribute('id', 'coFounder-name');
+      input.setAttribute('name', 'coFounder-name');
       coFounderInput.appendChild(input).classList.add('bg-light-gray','ba', 'b--black-20','pa2','mb2','w-100','br2','db');
       console.log(childNumber);
 
