d9dc9be12eae29eed1f97f6a184ab41aaabcbbee
Mario <mario.saliba98@gmail.com>
2017-08-16 16:31:04 +0300

    setup s3 for storing startups logos rel #151

---
+++ src/routes/aws_s3.js
@@ -0,0 +1,30 @@
+const aws = require('aws-sdk');
+aws.config.region = 'us-west-2';
+
+const S3_BUCKET = process.env.S3_BUCKET;
+
+module.exports = (req, res) => {
+  const s3 = new aws.S3();
+const fileName = req.query['file-name'];
+const fileType = req.query['file-type'];
+const s3Params = {
+  Bucket: S3_BUCKET,
+  Key: fileName,
+  Expires: 60,
+  ContentType: fileType,
+  ACL: 'public-read'
+};
+
+s3.getSignedUrl('putObject', s3Params, (err, data) => {
+  if(err){
+    console.log(err);
+    return res.end();
+  }
+  const returnData = {
+    signedRequest: data,
+    url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+  };
+  res.write(JSON.stringify(returnData));
+  res.end();
+});
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ const allEvents = require('./all_events.js');
 const newsHandler = require('./news.js');
 const allNews = require('./all_news.js');
 const event = require('./event.js');
+const awsS3 = require('./aws_s3.js');
 
 router.get('/', searchHome);
 router.post('/registerStartup', registerStartup);
@@ -19,5 +20,6 @@ router.get('/allEvents', allEvents);
 router.get('/news/:id', newsHandler);
 router.get('/allNews', allNews);
 router.get('/event/:id', event);
+router.get('/sign-s3', awsS3);
 
 module.exports = router;
