39117dc464fea06ad5644671c92fd278dd3fb4f9
suha-edris <suha.edris@gmail.com>
2017-08-17 09:29:48 +0300

    change input name

--- public/form.js
+++ public/form.js
@@ -4,10 +4,10 @@ var childNumber = 0;
 plusCircle.addEventListener('click', function () {
   var childNumber = document.getElementById('co-founder').childElementCount;
     if(childNumber <= 4) {
-      var input = document.createElement('input');
-      input.setAttribute('id', 'coFounder-name');
-      input.setAttribute('name', 'coFounder-name');
-      coFounderInput.appendChild(input).classList.add('bg-light-gray','ba', 'b--black-20','pa2','mb2','w-100','br2','db');
+      var newCoFounder = document.createElement('input');
+      newCoFounder.setAttribute('id', 'coFounder-name');
+      newCoFounder.setAttribute('name', 'coFounder-name');
+      coFounderInput.appendChild(newCoFounder).classList.add('bg-light-gray','ba', 'b--black-20','pa2','mb2','w-100','br2','db');
 
     }
 })
