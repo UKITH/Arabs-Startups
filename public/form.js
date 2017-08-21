68fea3815f2fec7aece333797cc3bdfa52cc040a
suha-edris <suha.edris@gmail.com>
2017-08-21 12:43:50 +0300

    add cross icon on click and function to remove element

--- public/form.js
+++ public/form.js
@@ -3,13 +3,21 @@ var coFounderInput = document.querySelector('#co-founder');
 var childNumber = 0;
 plusCircle.addEventListener('click', function () {
   var childNumber = document.getElementById('co-founder').childElementCount;
-    if(childNumber <= 4) {
+    if(childNumber <= 8) {
       var newCoFounder = document.createElement('input');
       newCoFounder.setAttribute('id', 'coFounder-name');
       newCoFounder.setAttribute('name', 'coFounder-name');
+      newCoFounder.setAttribute('required', '');
       coFounderInput.appendChild(newCoFounder).classList.add('ba', 'b--black-20','pa2','mb2','w-100','br2','db');
-
+      var removeCoFounder = document.createElement('img');
+      removeCoFounder.setAttribute('id', 'cross-circle');
+      removeCoFounder.setAttribute('src', './assets/cross.png');
+      coFounderInput.appendChild(removeCoFounder).classList.add('w2', 'h2','fr');
     }
+    removeCoFounder.addEventListener('click', function () {
+      removeCoFounder.parentNode.removeChild(newCoFounder);
+      removeCoFounder.parentNode.removeChild(removeCoFounder);
+    })
 })
 
 var bgChangeColor = document.querySelector('#navbar');
