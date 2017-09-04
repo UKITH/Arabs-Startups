fc340ae1e802d9d4c72f4ab61a11c7280714d5e8
Mario <mario.saliba98@gmail.com>
2017-09-04 18:49:47 +0300

    modifies the namimg of the collection

--- database/events_schema.js
+++ database/events_schema.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 
 const eventsSchema = mongoose.Schema({
   eventTitle: {type: String, required: true},
-  eventDate: {type: Date, min: Date.now(), required: true},
+  eventDate: {type: String, required: true},
   eventTime: {type: String, required: true},
   eventDescription: {type: String, required: true},
   eventOrganiser: {type: String, required: true},
   eventAddress: {type: String, required: true}
 });
 
-const mockEvents = mongoose.model('Mock Events', eventsSchema);
-const events = mongoose.model('Events Collection', eventsSchema);
+const events = mongoose.model('UnOfficial Events', eventsSchema);
+const officialEvents = mongoose.model('Official Events Collection', eventsSchema);
 
 module.exports = {
-  mockEvents,
+  officialEvents,
   events
 };
