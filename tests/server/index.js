3fcec8562ec8f839db2e79234571c72a6019fd07
suha-edris <suha.edris@gmail.com>
2017-09-07 10:10:17 +0300

    change var to const for consistency

--- tests/server/index.js
+++ tests/server/index.js
@@ -99,8 +99,8 @@ tape('Test the submit message page', (t) => {
 tape('Test for random startups page', (t) => {
   supertest(server).get('/randomStartups').end((err,res) => {
     t.error(err, 'No Error');
-    var testedClass = '<div class="dib mh1 tc">';
-    var count = (res.text.match(/<div class="dib mh1 tc">/g) || []).length;
+    const testedClass = '<div class="dib mh1 tc">';
+    const count = (res.text.match(/<div class="dib mh1 tc">/g) || []).length;
     t.ok(res.text.includes(testedClass), 'should have this class at least 1');
     t.equal(count, 10, 'should have only 10 startups');
     t.end();
